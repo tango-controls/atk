d35dd21108069410ca8d55ac47c789912fcc016d
JeanLucPons <pons@esrf.fr>
2013-09-25 13:16:09 +0000

    Removed modification for decimal separator management and forced Locale to EN_US

--- src/fr/esrf/tangoatk/widget/util/chart/JLAxis.java
+++ src/fr/esrf/tangoatk/widget/util/chart/JLAxis.java
@@ -310,11 +310,13 @@ public class JLAxis implements java.io.Serializable {
     minTickStep = 50.0;
     fitXAxisToDisplayDuration = true;
 
+    /* Removed because the followings calls ignore the local JVM setting made by Locale.setDefault()
     try {
       DecimalFormat format=(DecimalFormat)DecimalFormat.getInstance();
       DecimalFormatSymbols symbols=format.getDecimalFormatSymbols();
       decimalSeparator=symbols.getDecimalSeparator();
     } catch(Exception e) {}
+    */
 
   }
 
