17f9ad9561816f488cd6a9979b5b5437836753d9
poncet <poncet@4e9c00fd-8f2e-0410-aa12-93ce3db5e235>
2015-04-14 16:42:44 +0000

    The private field "theAxis" is now protected so that the subclasses can access the JLAxis element.

--- gui/atk/trunk/src/fr/esrf/tangoatk/widget/util/jdraw/JDAxis.java
+++ gui/atk/trunk/src/fr/esrf/tangoatk/widget/util/jdraw/JDAxis.java
@@ -93,7 +93,7 @@ public class JDAxis extends JDRectangular {
 
   // Private vars
   private int     fAscent;
-  private JLAxis  theAxis;
+  protected JLAxis  theAxis;
 
   // -----------------------------------------------------------
   // Construction
