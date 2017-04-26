f065e01397d5ec54243506aefb6762ab0794a37d
pons <pons@esrf.fr>
2017-04-26 08:08:34 +0200

    Fix bug in NumberSpectrumEvent getStandardValue

--- core/src/main/java/fr/esrf/tangoatk/core/NumberSpectrumEvent.java
+++ core/src/main/java/fr/esrf/tangoatk/core/NumberSpectrumEvent.java
@@ -19,7 +19,7 @@
  *  You should have received a copy of the GNU Lesser General Public License
  *  along with Tango.  If not, see <http://www.gnu.org/licenses/>.
  */
- 
+
 // File:          NumberSpectrumEvent.java
 // Created:       2002-01-31 17:49:20, assum
 // By:            <assum@esrf.fr>
@@ -31,91 +31,88 @@
 package fr.esrf.tangoatk.core;
 
 public class NumberSpectrumEvent extends ATKEvent {
-    double[]    value;
-    double[]    devValue;
+  double[] value;
+  double[] devValue;
 
-    public NumberSpectrumEvent(INumberSpectrum source, double[] value, long timeStamp)
-    {
-	super(source, timeStamp);
-	setValue(value);
-	devValue = new double[1];
+  public NumberSpectrumEvent(INumberSpectrum source, double[] value, long timeStamp) {
+    super(source, timeStamp);
+    setValue(value);
+    devValue = new double[1];
+  }
+
+  // Return the value in the display unit
+  public double[] getValue() {
+    return value;
+  }
+
+
+  // Return the value in the device server unit
+  public double[] getDeviceValue() {
+
+    double dispUnitFactor = 1.0;
+
+
+    if (source != null)
+      if (source instanceof INumberSpectrum)
+        dispUnitFactor = ((INumberSpectrum) source).getDisplayUnitFactor();
+
+    if (dispUnitFactor <= 0)
+      dispUnitFactor = 1.0;
+
+
+    if (dispUnitFactor == 1.0)
+      return value;
+
+    int dim = value.length;
+    if (dim != devValue.length || devValue.length == 0) {
+      devValue = new double[dim];
     }
 
-    // Return the value in the display unit
-    public double[] getValue() {
-	return value;
+    for (int i = 0; i < dim; i++) {
+      devValue[i] = value[i] / dispUnitFactor; //return the value in the device server unit
     }
 
+    return devValue;
+  }
 
-    // Return the value in the device server unit
-    public double[] getDeviceValue()
-    {
-	double      dispUnitFactor = 1.0;
-	
-	
-	if (source != null)
-	   if (source instanceof INumberSpectrum)
-	      dispUnitFactor = ((INumberSpectrum) source).getDisplayUnitFactor();
-	      
-	if (dispUnitFactor <= 0)
-	   dispUnitFactor = 1.0;
-	   
-	   
-	if (dispUnitFactor == 1.0)
-	   return value;
+  // Return the value in the standard unit
+  public double[] getStandardValue() {
 
-	int dim = value.length;
-	if (dim != devValue.length || devValue.length == 0)
-	{
-	    devValue = new double[dim];
-	}
+    double[] devVal;
+    double[] stdVal;
+    double stdUnitFactor = 1.0;
 
-	for (int i = 0; i < dim; i++)
-	{
-           devValue[i] = value[i] / dispUnitFactor; //return the value in the device server unit
-	}
-	return devValue;
+    devVal = getDeviceValue(); // First get the value in the device server unit
+
+    if (source != null)
+      if (source instanceof INumberSpectrum )
+        stdUnitFactor = ((INumberSpectrum ) source).getStandardUnitFactor();
+
+    if (stdUnitFactor <= 0)
+      stdUnitFactor = 1.0;
+
+    if (stdUnitFactor == 1.0)
+      return devVal;
+
+    int dim = devVal.length;
+    stdVal = new double[dim];
+
+    for (int i = 0; i < dim; i++) {
+      stdVal[i] = devVal[i] * stdUnitFactor; //return the value in the standard unit
     }
+    return stdVal;
+  }
 
-    // Return the value in the standard unit
-    public double[] getStandardValue()
-    {
-	double[]  devVal;
-        double[]  stdVal;
-        double    stdUnitFactor = 1.0;
-	
-        devVal = getDeviceValue(); // First get the value in the device server unit
-	
-	if (source != null)
-	   if (source instanceof INumberImage)
-	      stdUnitFactor = ((INumberImage) source).getStandardUnitFactor();
-	      
-	if (stdUnitFactor <= 0)
-	   stdUnitFactor = 1.0;
-	   
-	if (stdUnitFactor == 1.0)
-	   return devVal;
+  public void setValue(double[] value) {
+    this.value = value;
+  }
 
-	int dim = devVal.length;
-	stdVal = new double[dim];
+  public void setSource(INumberSpectrum source) {
+    this.source = source;
+  }
 
-	for (int i = 0; i < dim; i++)
-	{
-           stdVal[i] = devVal[i] * stdUnitFactor; //return the value in the standard unit
-	}
-	return stdVal;
-    }
 
-    public void setValue(double []value) {
-	this.value = value;
-    }
-
-    public void setSource(INumberSpectrum source) {
-	this.source = source;
-    }
-
-    
-    public String getVersion() {
-	return "$Id$";
-    }
+  public String getVersion() {
+    return "$Id$";
+  }
 }
